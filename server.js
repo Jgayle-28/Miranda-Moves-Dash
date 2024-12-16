e2312aa0668b084acb726990753afa88476a5804
Jerehme Gayle <jerehme.gayle@gmail.com>
2024-12-16 13:12:27 -0700

    chore: updated package.json

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
+const express = require('express')
+const connectDB = require('./config/db')
+const path = require('path')
 
-const app = express();
+const app = express()
 
 //Connect DB
-connectDB();
+connectDB()
 
 //Init middleware
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }))
 
 // app.get('/', (req, res) => res.json({ msg: 'Welcome to Movely' }));
 
 //Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('client/build'))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  )
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8800
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
